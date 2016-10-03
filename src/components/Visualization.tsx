8dde9e4eb25000bb83616e1f41a5c2cc8e27070e
Andras Nemeth <cucu.szpem@gmail.com>
2016-10-03 23:45:36 +0200

    [visualization] invert and use age categories

--- src/components/Visualization.tsx
+++ src/components/Visualization.tsx
@@ -5,16 +5,40 @@ import { SimState } from '../index';
 import { State } from '../model';
 
 
+const pad = (num: number) => {
+  const str = num.toString();
+  if (str.length < 2) {
+    return ' ' + str;
+  }
+  return str;
+};
+
+const getAgeCats = (pyramid: number[]) => {
+  let ageCats: number[] = [];
+  pyramid.forEach((pop: number, age: number) => {
+    const ageCat = Math.floor(age / 5);
+    if (ageCats.length <= ageCat) {
+      ageCats.push(0);
+    }
+    ageCats[ageCat] += pop;
+  });
+  return ageCats;
+};
+
 interface VisualizationProps {
   state: State;
 }
 
 const VisualizationComponent = ({ state }: VisualizationProps) => {
+  const ageCats = getAgeCats(state.pyramid);
+  const maxAgeCat = ageCats.length - 1;
+  const totalPop = state.pyramid.reduce((a: number, b: number) => a + b, 0);
   return <div>
-    <p>Total population of year {state.year} = {Math.round(state.pyramid.reduce((a: number, b: number) => a + b, 0))}</p>
+    <p>Total population of year {state.year} = {Math.round(totalPop)}</p>
     <pre>
-      {state.pyramid.map((pop: number, age: number) => {
-        return age + ' | ' + '.'.repeat(pop / 10) + ' ' + Math.round(pop) + '\n';
+      {ageCats.reverse().map((pop: number, revAgeCat: number) => {
+        const ageCat = maxAgeCat - revAgeCat;
+        return pad(5 * ageCat) + '-' + pad(5 * ageCat + 4) + ' | ' + '='.repeat(pop / 100) + ' ' + Math.round(pop) + '\n';
       })}
     </pre>
   </div>;
